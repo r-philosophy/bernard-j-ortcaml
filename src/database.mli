b4b1cc6e20781826543bb78d2bb18126e7b70f63
Levi Roth <levimroth@gmail.com>
2020-10-04 08:47:16 -0400

    Move Database_intf into database.mli

--- src/database.mli
+++ src/database.mli
@@ -1,1 +1,26 @@
-include Database_intf.Database
+open! Core
+open! Async
+open! Import
+
+type t
+
+val create : database:string -> t Deferred.t
+val already_acted : t -> target:Action.Target.t -> moderator:Username.t -> bool Deferred.t
+
+val log_rule_application
+  :  t
+  -> target:Action.Target.t
+  -> action_summary:string
+  -> author:Username.t option
+  -> moderator:Username.t
+  -> subreddit:Thing.Subreddit.Id.t
+  -> time:Time_ns.t
+  -> unit Deferred.t
+
+val update_subscriber_counts : t -> subreddits:Thing.Subreddit.t list -> unit Deferred.t
+
+val update_moderator_table
+  :  t
+  -> moderators:Username.t list
+  -> subreddit:Thing.Subreddit.Id.t
+  -> unit Deferred.t
