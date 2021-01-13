915751a650a3ab6ee85334b6dc531f63b211604f
Levi Roth <levimroth@gmail.com>
2021-01-12 19:12:36 -0500

    Update to reddit_api 8f6f80c237213765215cce34ea5f395923b03a57

--- src/import.ml
+++ src/import.ml
@@ -5,11 +5,10 @@ include Reddit_api_async
 let retry_or_fail retry_manager here endpoint =
   match%bind Retry_manager.call retry_manager endpoint with
   | Ok v -> return v
-  | Error (response, body) ->
+  | Error error ->
     raise_s
       [%message
         "Reddit returned error"
           (here : Source_code_position.t)
-          (response : Cohttp.Response.t)
-          (body : Cohttp.Body.t)]
+          (error : Retry_manager.Non_transient_error.t)]
 ;;
