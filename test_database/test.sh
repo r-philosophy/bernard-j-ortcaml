c301b7631585d9cff88d6c1396a55be41a6c80d7
Levi Roth <levimroth@gmail.com>
2021-10-03 13:11:52 -0400

    Simplify database test action

--- test_database/test.sh
+++
@@ -1,4 +0,0 @@
-dropdb testing
-createdb testing
-psql -d testing -f create_tables.sql
-./test_database/main.exe -database testing -link test_database/link.json  -subreddit test_database/subreddit.json
