0d03d9065e61bd03fb3b883ee8e8b1f3d38e43d1
Levi Roth <levimroth@gmail.com>
2022-07-03 19:32:32 -0400

    Retry when rate limited

--- src/import.ml
+++ src/import.ml
@@ -4,15 +4,29 @@ include Reddit_api_async
 module Time_ns = Time_ns_unix
 
 let retry_or_fail retry_manager here endpoint =
-  match%bind Retry_manager.call retry_manager endpoint with
-  | Ok v -> return v
-  | Error error ->
-    raise_s
-      [%message
-        "Reddit returned error"
-          (here : Source_code_position.t)
-          ~request:(endpoint.request : Endpoint.Request.t)
-          (error : Retry_manager.Permanent_error.t)]
+  Deferred.repeat_until_finished () (fun () ->
+      match%bind Retry_manager.call retry_manager endpoint with
+      | Ok v -> return (`Finished v)
+      | Error
+          (Endpoint_error
+            (Json_response_errors
+              [ { error = "RATELIMIT"
+                ; fields = [ "ratelimit" ]
+                ; details =
+                    "Looks like you've been doing that a lot. Take a break for 5 seconds \
+                     before trying again."
+                ; error_type = _
+                }
+              ])) ->
+        let%bind () = Clock_ns.after (Time_ns.Span.of_int_sec 6) in
+        return (`Repeat ())
+      | Error error ->
+        raise_s
+          [%message
+            "Reddit returned error"
+              (here : Source_code_position.t)
+              ~request:(endpoint.request : Endpoint.Request.t)
+              (error : Retry_manager.Permanent_error.t)])
 ;;
 
 (** A [Scope.t] represents an OAuth2 scope. *)
