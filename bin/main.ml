fb2e281b2c6698d91c5c09eb9e3cb50af1ea96d9
Levi Roth <levimroth@gmail.com>
2023-08-26 18:11:17 -0400

    Upgrade to a431ddcbbc91a4ace19de49229eb933bc8604706

--- bin/main.ml
+++ bin/main.ml
@@ -3,5 +3,6 @@ open! Async
 
 let () =
   Memtrace.trace_if_requested ();
+  Log.set_output Reddit_api_async.Logging.log (Log.get_output (force Log.Global.log));
   Command_unix.run Bernard_j_ortcutt.Bot.command
 ;;
--- src/action.ml
+++ src/action.ml
@@ -167,12 +167,7 @@ let nuke (target : Target.t) ~retry_manager =
   let%bind comment_response =
     Utils.retry_or_fail retry_manager [%here] (Endpoint.comments ?comment () ~link)
   in
-  let pipe =
-    Iter_comments.iter_comments
-      retry_manager
-      ~log:(force Log.Global.log)
-      ~comment_response
-  in
+  let pipe = Iter_comments.iter_comments retry_manager ~comment_response in
   Pipe.iter pipe ~f:(fun comment ->
       let id = `Comment (Thing.Comment.id comment) in
       Utils.retry_or_fail retry_manager [%here] (Endpoint.remove ~id ~spam:false))
